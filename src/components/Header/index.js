e063f2cfea614d0bb5fea96d70f38b6685bafcb4
ethan <ethannjw@gmail.com>
2021-05-29 21:24:39 +0800

    removed unused vars

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,6 @@ import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import IconButton from '@material-ui/core/IconButton';
 
